fe39762eebc3999ee4dd67976509974d6e581b18
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-05 06:40:29 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     server: {
-        port: 8087,
+        port: 5000,
         debug: false,
         version: '1.0',
         useHttps: false, // Default: false
     },
     proxy: {
-        scrape: true, // Default: 'false' | Set to true to enable proxy scraping | false use proxies.txt
+        scrape: false, // Default: 'false' | Set to true to enable proxy scraping | false use proxies.txt
         protocol: 'http', // Default: 'http' | Proxy protocol, can be 'http', 'https', 'socks4', 'socks5'
         timeout: 5000
     },
